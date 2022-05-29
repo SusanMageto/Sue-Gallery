48fe53970e10e36a86c1975060c56e70d5391cdd
SusanMageto <susan.mageto@student.moringaschool.com>
2022-05-30 02:39:49 +0300

    makes models for the image and the category

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -76,8 +76,8 @@ WSGI_APPLICATION = 'photoshare.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'nikon',
     }
 }
 
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
